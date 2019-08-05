eb1a669814788adcb4278c487747e567a097cdf3
Kevin <kevintg1212@gmail.com>
2019-08-05 13:38:07 +0700

    sec

---
+++ resources/views/layouts/slider.blade.php
@@ -0,0 +1,35 @@
+<!-- Slide1 -->
+<section class="slide1">
+    <div class="wrap-slick1">
+        <div class="slick1">
+        </div>
+    </div>
+</section>
+
+<!-- Slide1 -->
+<section class="slide1">
+    <div class="wrap-slick1">
+        <div class="slick1">
+            <div class="item-slick1 item1-slick1"
+                 style="background-image: url({{ asset('assets/images/master-slide-04.jpg)') }}">
+                <div class="wrap-content-slide1 sizefull flex-col-r-m p-l-0a p-r-0 p-t-280 p-b-170"
+                     style="position:relative;  white-space: nowrap;  overflow: hidden;">
+                    <div class="blur">
+                        <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37"
+                            data-appear="lightSpeedIn">
+                            LOREM IPSUM
+                        </h2>
+                        <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15"
+                              data-appear="rollIn">
+									AUTO PARTS SOLUTION
+								</span>
+
+                        <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
