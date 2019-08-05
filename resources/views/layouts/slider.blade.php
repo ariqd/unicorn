3e22a096921c2e2a14d17e22604b7aafa4412f35
Ariq Daffa <ariqdaffaathallah@gmail.com>
2019-08-05 17:08:13 +0700

    frontend beres

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -1,36 +1,42 @@
-
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#Carousel" data-slide-to="0" class="active"></li>
-      <li data-target="#Carousel" data-slide-to="1"></li>
-      <li data-target="#Carousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="assets/images/slide1.png" alt="Slide1" style="width:100%;">
-      </div>
-
-      <div class="item">
-        <img src="assets/images/slide1.png" alt="Slide2" style="width:100%;">
-      </div>
-
-      <div class="item">
-        <img src="assets/images/slide1.png" alt="Slide3" style="width:100%;">
-      </div>
+<div class="container pt-4">
+    <div class="bd-example">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('assets/images/slide1.png') }}" class="d-block w-100 img-fluid" alt="carousel">
+                    <div class="carousel-caption bg-white mb-5 p-3 text-dark text-left d-none d-md-block">
+                        <h2>First slide label</h2>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/images/slide1.png') }}" class="d-block w-100 img-fluid" alt="carousel">
+                    <div class="carousel-caption bg-white mb-5 p-3 text-dark text-left d-none d-md-block">
+                        <h2>Second slide label</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/images/slide1.png') }}" class="d-block w-100 img-fluid" alt="carousel">
+                    <div class="carousel-caption bg-white mb-5 p-3 text-dark text-left d-none d-md-block">
+                        <h2>Third slide label</h2>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
 </div>
