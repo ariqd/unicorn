14b8feed8b9fe5ddaf10ea6b4da9bbb19d3a0096
Alex Hendrawan <alex.hendrawan444@gmail.com>
2019-08-28 11:30:53 +0700

    driver

---
+++ engine/resources/views/admin/driver/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.carbon')
+
+@section('title')
+Driver
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"></div>
+
+                <div class="card-body">
+                        <div class="form-group form-group-default">
+                            <label>Name</label>
+                        </div>
+
+                        <div class="form-group form-group-default">
+                            <label>Username</label>
+                        </div>
+
+                        <div class="form-group form-group-default">
+                            <label>Telephone</label>
+                        </div>
+
+                        <div class="form-group form-group-default">
+                            <label>Email</label>
+                        </div>
+
+                        <div class="form-group form-group-default">
+                            <label>Picture</label>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
